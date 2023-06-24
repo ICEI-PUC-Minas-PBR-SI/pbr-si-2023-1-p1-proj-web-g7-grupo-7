89f627c56f28b03a59d15e783ff60d948b94d70e
Elenice <elenicerof@gmail.com>
2023-06-24 10:07:59 -0300

    Atualizar estoque.html e estoque.js

--- src/Codigo/estoque.js
+++ src/Codigo/estoque.js
@@ -1,23 +1,46 @@
-
-// Função para buscar os dados da página cadastrarprodutos.html
-function buscarDados() {
-  // Buscando os input da página cadastrarprodutos.html
-  var codigo = document.getElementById('codigo').value;
-  var produto = document.getElementById('produto').value;
-  var categoria = document.getElementById('categoria').value;
-  var marca = document.getElementById('marca').value;
-  var preco = document.getElementById('preco').value;
-  var estoque = document.getElementById('estoque').value;
-
-  // Exibir os dados no console 
-  console.log('Código:', codigo);
-  console.log('Produto:', produto);
-  console.log('Categoria:', categoria);
-  console.log('Marca:', marca);
-  console.log('Preço de Venda:', preco);
-  console.log('Estoque:', estoque);
+// Função para buscar os dados do Local Storage
+function leDados() {
+  let dados = localStorage.getItem('db');
+  if (dados) {
+    return JSON.parse(dados);
+  } else {
+    // Retorna um objeto vazio caso não haja dados no Local Storage
+    return {
+      cadastro: []
+    };
+  }
 }
 
-// Associar a função buscarDados ao botão "Buscar"
-var buscarButton = document.getElementById('buscar');
-buscarButton.addEventListener('click', buscarDados);
+// Função para buscar um produto pelo código
+function buscarProduto() {
+  let codigo = document.getElementById('codigo').value;
+  let objDados = leDados().cadastro;
+
+  for (let i = 0; i < objDados.length; i++) {
+    for (let j = 0; j < objDados[i].produto.length; j++) {
+      if (objDados[i].produto[j].codigo === codigo) {
+        let produto = objDados[i].produto[j];
+        document.getElementById('produto').value = produto.nome;
+        document.getElementById('categoria').value = produto.categoria;
+        document.getElementById('marca').value = produto.marca;
+        document.getElementById('preco').value = produto.preco;
+        document.getElementById('estoque').value = produto.estoque;
+        document.getElementById('img').value = produto.img;
+        document.getElementById('detalhes').value = produto.detalhes;
+        return;
+      }
+    }
+  }
+
+  // Se o produto não foi encontrado, limpa os campos
+  document.getElementById('produto').value = '';
+  document.getElementById('categoria').value = '';
+  document.getElementById('marca').value = '';
+  document.getElementById('preco').value = '';
+  document.getElementById('estoque').value = '';
+  document.getElementById('img').value = '';
+  document.getElementById('detalhes').value = '';
+}
+
+//botão de busca
+document.getElementById('buscar').addEventListener('click', buscarProduto);
