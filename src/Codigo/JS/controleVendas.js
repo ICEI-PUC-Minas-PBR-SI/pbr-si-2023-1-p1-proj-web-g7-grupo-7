a80c618fd9b6969df08ad6057f4c9147d064fe22
AlexsanderRLopes <alexrolopes18@gmail.com>
2023-07-03 09:33:59 -0300

    atualização na tela do relatorio de vendas

--- src/Codigo/JS/controleVendas.js
+++ src/Codigo/JS/controleVendas.js
@@ -49,56 +49,114 @@ function mostrarVendas () {
     section.innerHTML = str;
   }
 
+  function relatorioCarrinho () {
+
+    $(section).html("");
+    let objDados = dadosCarrinho();
+    objDados = objDados.carrinho;
+    for (let i = 0; i < 1; i++) {
+    let total = parseFloat(objDados[i].preco) * parseFloat(objDados[i].qtd);
+    $(section).append(`<div class="row">
+    <div class="col">
+        <p>${objDados[i].id}</p>
+    </div>
+    <div class="col">
+        <p>${geraCodigoVenda()}</p>
+    </div>
+    <div class="col">
+        <p>R$ ${total}</p>
+    </div>
+    <div class="col">
+        <button id="btnRVendas"onclick="mostrarDetalhes()">Detalhes</button>
+    </div>
+   </div>`);
+
+    }
+  }
+
+  function dadosCarrinho() {
+    let strDados = localStorage.getItem('db_carrinho');
+    let objDados = {};
+  
+    if (strDados) {
+        objDados = JSON.parse(strDados);
+    }
+    else {
+      objDados = {
+        carrinho: [
+          /*{id: "001", codigo: "001", nome: "cerveja IPA", preco: "5,00",qtd: "5", img: "https://source.unsplash.com/random/500x500?sing=1"},
+          {id: "001", codigo: "002", nome: "cerveja Pilsen", preco: "4,00",qtd: "5", img: "https://source.unsplash.com/random/500x500?sing=2"},
+          {id: "001", codigo: "003", nome: "cerveja Weiss", preco: "6,00",qtd: "5", img: "https://source.unsplash.com/random/500x500?sing=3"},
+          {id: "001", codigo: "004", nome: "cerveja Lager", preco: "3,00",qtd: "5", img: "https://source.unsplash.com/random/500x500?sing=4"},
+        {id: "002", codigo: "001", nome: "refrigerante COCA COLA", preco: "4,00",qtd: "5", img: "https://source.unsplash.com/random/500x500?sing=5"}*/]
+      }
+    }
+  
+    return objDados;
+  }
+
   //Função para mostrar os detalhes quando clicar no item
 function mostrarDetalhes () {
 
-  let detalhes = document.getElementById('detalhes')
+    $(infoDetalhes).html("");
+    $(infoDetalhes).append(`<div class="vendas">
+    <div class="menu">
+       <div class="row">
+           <div class="col">
+               <h4>ID do Fornecedor</h4>
+           </div>
+           <div class="col">
+               <h4>Código do produto</h4>
+           </div>
+           <div class="col">
+               <h4>Produto</h4>
+           </div>
+           <div class="col">
+               <h4>Quantidade</h4>
+           </div>
+           <div class="col">
+               <h4>Preço Unitário</h4>
+           </div>
+           <div class="col">
+               <h4>Total</h4>
+           </div>
+          </div>
+    </div>`);
 
-  let dados = leDados();
-  dados = dados.cadastro;
-  let str = '';
-  console.log(dados);
-
-  let varCod = 1;
-
-  for (var i = 0; i < dados.length; i++) {
-
-    for (var j = 0; j <dados[i].produto.length; j++){
-
-      let data = numeroAleatorio(1, 31) + "/" + numeroAleatorio(1, 12) + "/2023";
-
-      str +=  `<div class="dados">
-      <table>
-          <thead>
-              <tr>
-                  <th>Id do Cliente</th>
-                  <th>Código de venda</th>
-                  <th>Categoria</th>
-                  <th>Marca</th>
-                  <th>Data da venda</th>
-                  <th>Valor do produto</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>${dados[i].id}</td>
-                  <td>${(varCod * 100) + i + j}</td>
-                  <td>${dados[i].produto[j].categoria}</td>
-                  <td>${dados[i].produto[j].marca}</td>
-                  <td>${data}</td>
-                  <td>${dados[i].produto[j].preco}</td>
-              </tr>
-              </table>
-              </div>`;
-
+    $(detalhes).html("");
+    //let tela = document.getElementById('tela');
+    let objDados = dadosCarrinho();
+    objDados = objDados.carrinho;
+    for (let i = 0; i < objDados.length; i++) {
+        let total = parseFloat(objDados[i].preco) * parseFloat(objDados[i].qtd);
+        $(detalhes).append(`<div class="row">
+        <div class="col">
+            <p>${objDados[i].id}</p>
+        </div>
+        <div class="col">
+            <p>${objDados[i].codigo}</p>
+        </div>
+        <div class="col">
+            <p>${objDados[i].nome}</p>
+        </div>
+        <div class="col">
+            <p>${objDados[i].qtd}</p>
+        </div>
+        <div class="col">
+            <p>${objDados[i].preco}</p>
+        </div>
+        <div class="col">
+            <p>R$ ${total}</p>
+        </div>
+       </div>`);
     }
 
-    varCod += 1;
+}
 
-    }
+function geraCodigoVenda() {
 
-    detalhes.innerHTML = str;
-
+    let codigo = 1;
+    return codigo;
 }
 
 //Aleatorizar um numero
